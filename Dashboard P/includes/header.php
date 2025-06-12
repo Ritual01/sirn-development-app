ab323e0bdbf343ad14bbef7ff257d43d7157f11d
lenivor <111296592+lenivor@users.noreply.github.com>
2025-06-12 15:12:34 -0500

    header.php

--- Dashboard P/includes/header.php
+++ Dashboard P/includes/header.php
@@ -4,10 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard - Monitoreo de Calidad del Agua</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"  rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
+    <!-- AOS para animaciones -->
+    <link href="https://cdn.jsdelivr.net/npm/aos@2.3.4/dist/aos.css" rel="stylesheet">
+<!-- DataTables -->
+     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+
 </head>
 <body>
-<header class="bg-primary text-white p-3 mb-4">
-    <h1 class="text-center">Sistema de Monitoreo de Calidad del Agua</h1>
-</header>v
+    <header class="header-principal text-white p-4 mb-4">
+        <div class="contenedor-central">
+            <h1 class="titulo-principal">Sistema de Monitoreo de Calidad del Agua</h1>
+        </div>
+    </header>
